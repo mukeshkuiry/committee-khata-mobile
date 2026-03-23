67e5d91c33306d4c234fb639ff54334ebda6b23f
mukeshkuiry <mukeshkk3162@gmail.com>
2026-03-24 02:39:06 +0530

    Update config anf names

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,5 +2,5 @@
 // For production, replace with EAS Updates env or Expo config plugins.
 
 export const config = {
-  API_BASE_URL: 'http://192.168.1.57:4000/api/v1',
+  API_BASE_URL: 'https://committee-api-wgek.onrender.com/api/v1',
 };
